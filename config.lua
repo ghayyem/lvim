e5ab8ee1845ff241288bdf6b2b5fb91210df1ab2
ahrar <ahrar@deriv.com>
2022-08-30 19:08:37 +0800

    new changes

--- config.lua
+++ config.lua
@@ -16,9 +16,18 @@ lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
-
 lvim.builtin.dap.active = true
+lvim.builtin.telescope.defaults = {
+  path_display = { "truncate" },
+}
+
+lvim.builtin.telescope.on_config_done = function(telescope)
+  pcall(telescope.load_extension, "flutter")
+end
+lvim.builtin.treesitter.rainbow.enable = true
+
+lvim.builtin.terminal.direction = "horizontal"
+
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
@@ -42,25 +51,13 @@ lvim.plugins = {
   { 'akinsho/flutter-tools.nvim',
     requires = 'nvim-lua/plenary.nvim' },
   { 'sainnhe/everforest' },
-  {
-    "tpope/vim-fugitive",
-    cmd = {
-      "G",
-      "Git",
-      "Gdiffsplit",
-      "Gread",
-      "Gwrite",
-      "Ggrep",
-      "GMove",
-      "GDelete",
-      "GBrowse",
-      "GRemove",
-      "GRename",
-      "Glgrep",
-      "Gedit"
-    },
-    ft = { "fugitive" }
-  },
+  { "rcarriga/nvim-dap-ui" },
+  { "catppuccin/nvim", as = "catppuccin" },
+  { "doums/darcula" },
+  { 'mhartington/oceanic-next' },
+  { "Mofiqul/dracula.nvim" },
+  { 'shaunsingh/nord.nvim' },
+  { 'shaunsingh/moonlight.nvim' },
   {
     "folke/persistence.nvim",
     event = "BufReadPre", -- this will only start session saving when an actual file was opened
@@ -75,15 +72,108 @@ lvim.plugins = {
       require("auto-save").setup()
     end,
   },
+  {
+    "p00f/nvim-ts-rainbow",
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require "lsp_signature".on_attach() end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "karb94/neoscroll.nvim",
+    event = "WinScrolled",
+    config = function()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
+          '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+        hide_cursor = true, -- Hide cursor while scrolling
+        stop_eof = true, -- Stop at <EOF> when scrolling downwards
+        use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+        respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+        cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+        easing_function = nil, -- Default easing function
+        pre_hook = nil, -- Function to run before the scrolling animation starts
+        post_hook = nil, -- Function to run after the scrolling animation ends
+      })
+    end
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    event = { "BufRead", "BufNew" },
+    config = function()
+      require("bqf").setup({
+        auto_enable = true,
+        preview = {
+          win_height = 12,
+          win_vheight = 12,
+          delay_syntax = 80,
+          border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+        },
+        func_map = {
+          vsplit = "",
+          ptogglemode = "z,",
+          stoggleup = "",
+        },
+        filter = {
+          fzf = {
+            action_for = { ["ctrl-s"] = "split" },
+            extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+          },
+        },
+      })
+    end,
+  },
+  {
+    "windwp/nvim-spectre",
+    event = "BufRead",
+    config = function()
+      require("spectre").setup()
+    end,
+  },
+  {
+    "kevinhwang91/rnvimr",
+    cmd = "RnvimrToggle",
+    config = function()
+      vim.g.rnvimr_draw_border = 1
+      vim.g.rnvimr_pick_enable = 1
+      vim.g.rnvimr_bw_enable = 1
+    end,
+  },
 }
 
-lvim.builtin.treesitter.rainbow.enable = true
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Diagnostics",
+  t = { "<cmd>TroubleToggle<cr>", "trouble" },
+  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+}
 
-
+lvim.builtin.which_key.mappings["S"] = {
+  name = "Session",
+  c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
+  l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
+  Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
+}
 lvim.builtin.dap.on_config_done = function(dap)
   dap.adapters.dart = {
     type = "executable",
-    command = "node",
+    command = "flutter",
     args = { os.getenv('HOME') .. "/Dart-Code/out/dist/debug.js", "flutter" }
   }
   dap.configurations.dart = {
@@ -108,8 +198,10 @@ lvim.builtin.dap.on_config_done = function(dap)
     }
 
   }
-
 end
+
+require("dapui").setup()
+
 require("flutter-tools").setup {
   decorations = {
     statusline = {
@@ -125,7 +217,7 @@ require("flutter-tools").setup {
   debugger = { -- integrate with nvim dap + install dart code debugger
     enabled = true,
     run_via_dap = true, -- use dap instead of a plenary job to run flutter apps
-    register_configurations = function(paths)
+    register_configurations = function()
       require("dap").configurations.dart = {
         {
           dartSdkPath = os.getenv('Home') .. "/Development/flutter/bin/cache/dart-sdk/",
